cf6e26d70efb63e1254e33c6e399805ed3ea777d
Wesley <wesleyjoga500@gmail.com>
2019-02-12 12:07:37 -0200

    Aula notification

---
+++ app/Notifications/PokedexCheia.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use DB;
+
+class PokedexCheia extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $user = $notifiable;
+        $pokemon = DB::table('pokemon')->where('user_id',$user->id)->orderBy('nome','desc')->first();
+
+        return (new MailMessage)
+
+          ->greeting('Hello trainer!')
+          ->line('Your pokédex is almost full '.$user->qtdPokedex.' /100 Pokemons!')
+          ->line('The last pokemon added was: '.$pokemon->nome)
+          ->line("Go Catch 'Em All'")
+          ->line('Pokemon: '.$pokemon->nome);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
